8a516afa0559acae2d6d67a84a181023f37b3a35
sim696968 <zim957720@gmail.com>
2026-08-06 11:54:39 +0800

    new

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,24 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\Users\\Win11\\Downloads\\flutter_windows_3.44.1-stable\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\Win11\\Desktop\\Wedify" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\Users\\Win11\\Downloads\\flutter_windows_3.44.1-stable\\flutter"
+  "PROJECT_DIR=C:\\Users\\Win11\\Desktop\\Wedify"
+  "FLUTTER_ROOT=C:\\Users\\Win11\\Downloads\\flutter_windows_3.44.1-stable\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\Win11\\Desktop\\Wedify\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\Win11\\Desktop\\Wedify"
+  "FLUTTER_TARGET=C:\\Users\\Win11\\Desktop\\Wedify\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDQuMw==,RkxVVFRFUl9DSEFOTkVMPXN0YWJsZQ==,RkxVVFRFUl9HSVRfVVJMPWh0dHBzOi8vZ2l0aHViLmNvbS9mbHV0dGVyL2ZsdXR0ZXIuZ2l0,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZTFmZDk2M2M2Zg==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YTRjZTI1N2M2OA==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMi4y"
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\Win11\\Desktop\\Wedify\\.dart_tool\\package_config.json"
+)
